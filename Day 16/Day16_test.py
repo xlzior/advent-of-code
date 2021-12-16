4b14c745f4e6d50737ae824bc8bfdf4db3b971d2
xlzior <wenjun.lye@gmail.com>
2021-12-16 19:02:22 +0800

    Day 16 (extract out test cases)

---
+++ Day 16/Day16_test.py
@@ -0,0 +1,42 @@
+from Day16 import parse_and_evaluate
+
+# public tests cases
+part_1_tests = [
+    ("8A004A801A8002F478", 16),
+    ("620080001611562C8802118E34", 12),
+    ("C0015000016115A2E0802F182340", 23),
+    ("A0016C880162017C3686B18A3D4780", 31)
+]
+
+part_2_tests = [
+    ("C200B40A82", 3),
+    ("04005AC33890", 54),
+    ("880086C3E88112", 7),
+    ("CE00C43D881120", 9),
+    ("D8005AC2A8F0", 1),
+    ("F600BC2D8F", 0),
+    ("9C005AC2F8F0", 0),
+    ("9C0141080250320F1802104A08", 1)
+]
+
+HEADER = '\033[94m'
+GREEN = '\033[92m'
+RED = '\033[91m'
+ENDC = '\033[0m'
+
+print(f"{HEADER}Part 1 Public Test Cases{ENDC}")
+for sample, expected_version in part_1_tests:
+    observed_version, _ = parse_and_evaluate(sample)
+    if observed_version == expected_version:
+        print(f"{GREEN}✓ {sample} -> version sum: {expected_version}{ENDC}")
+    else:
+        print(f"{RED}✗ {sample} version sum; Expected: {expected_version}, received {observed_version}{ENDC}")
+
+print()
+print(f"{HEADER}Part 2 Public Test Cases{ENDC}")
+for sample, expected_value in part_2_tests:
+    _, observed_value = parse_and_evaluate(sample)
+    if observed_value == expected_value:
+        print(f"{GREEN}✓ {sample} -> value: {expected_value}{ENDC}")
+    else:
+        print(f"{RED}✗ {sample} value; Expected: {expected_value}, received {observed_value}{ENDC}")
