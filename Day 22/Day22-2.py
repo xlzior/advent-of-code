62342c2b0b4645e63cc8c87118d95bcae0ca3e13
xlzior <wenjun.lye@gmail.com>
2021-12-23 00:32:35 +0800

    Day 22 (refactor)
    Encapsulate start and end into a tuple

--- Day 22/Day22-2.py
+++ Day 22/Day22-2.py
@@ -3,36 +3,28 @@ import sys
 import re
 
 class Cuboid:
-    def __init__(self, start_x, end_x, start_y, end_y, start_z, end_z, is_on):
-        self.start_x = start_x
-        self.end_x = end_x
-        self.start_y = start_y
-        self.end_y = end_y
-        self.start_z = start_z
-        self.end_z = end_z
+    def __init__(self, x, y, z, is_on):
+        self.x = x
+        self.y = y
+        self.z = z
         self.is_on = is_on
 
     def intersect(self, other: Cuboid):
-        start_x = max(self.start_x, other.start_x)
-        start_y = max(self.start_y, other.start_y)
-        start_z = max(self.start_z, other.start_z)
-        end_x = min(self.end_x, other.end_x)
-        end_y = min(self.end_y, other.end_y)
-        end_z = min(self.end_z, other.end_z)
+        x = max(self.x[0], other.x[0]), min(self.x[1], other.x[1])
+        y = max(self.y[0], other.y[0]), min(self.y[1], other.y[1])
+        z = max(self.z[0], other.z[0]), min(self.z[1], other.z[1])
 
-        if start_x <= end_x and start_y <= end_y and start_z <= end_z:
-            return Cuboid(start_x, end_x, start_y, end_y, start_z, end_z, not other.is_on)
+        if x[0] <= x[1] and y[0] <= y[1] and z[0] <= z[1]:
+            return Cuboid(x, y, z, not other.is_on)
 
     def size(self):
         return (1 if self.is_on else -1) *\
-               (self.end_x - self.start_x + 1) *\
-               (self.end_y - self.start_y + 1) *\
-               (self.end_z - self.start_z + 1)
+               (self.x[1] - self.x[0] + 1) *\
+               (self.y[1] - self.y[0] + 1) *\
+               (self.z[1] - self.z[0] + 1)
 
 
-with open(sys.argv[1]) as file:
-    puzzle_input = file.read().strip().split("\n")
-
+puzzle_input = open(sys.argv[1]).read().strip().split("\n")
 originals = list()  # cuboids given in instructions
 cuboids = list()    # all cuboids, including intersections with each other
 count = 0
@@ -40,9 +32,9 @@ count = 0
 for line in puzzle_input:
     state = line.split(" ", 1)[0]
     start_x, end_x, start_y, end_y, start_z, end_z = map(int, re.findall(r"(-?\d+)", line))
-    originals.append(Cuboid(start_x, end_x, start_y, end_y, start_z, end_z, state == "on"))
+    originals.append(Cuboid((start_x, end_x), (start_y, end_y), (start_z, end_z), state == "on"))
 
-for original in originals:                          # add originals in one-by-one
+for original in originals:                          # consider / add originals in one-by-one
     for cuboid in cuboids.copy():                   # for each addition, check for intersections with all previous
         intersection = original.intersect(cuboid)
         if intersection:
