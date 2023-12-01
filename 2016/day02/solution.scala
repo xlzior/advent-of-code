fde0e6630a401eb96ab1a1fcafff08e5c0363622
xlzior <wenjun.lye@gmail.com>
2023-12-01 20:24:39 +0000

    2016 Day 2 refactor

--- 2016/day02/solution.scala
+++ 2016/day02/solution.scala
@@ -13,79 +13,75 @@ class Pair(val x: Int, val y: Int) {
   override def hashCode: Int = x.hashCode + y.hashCode
 }
 
+abstract class Keypad() {
+  var location: Pair
+  val keypad: Array[Array[String]]
+
+  var password: String = ""
+  val deltas = Map(
+    "U" -> new Pair(0, -1),
+    "R" -> new Pair(1, 0),
+    "D" -> new Pair(0, 1),
+    "L" -> new Pair(-1, 0)
+  )
+
+  def isValid(location: Pair): Boolean
+
+  def move(steps: String): Unit = {
+    location = steps.foldLeft(location)((location, step) => {
+      val newLocation = location + deltas(step.toString)
+      if (isValid(newLocation)) newLocation else location
+    })
+    password = password + keypad(location.y)(location.x)
+  }
+}
+
+class SquareKeypad() extends Keypad {
+  var location: Pair = new Pair(1, 1)
+
+  val keypad = Array(
+    Array("1", "2", "3"),
+    Array("4", "5", "6"),
+    Array("7", "8", "9")
+  )
+
+  def isValid(location: Pair): Boolean = {
+    location.x >= 0 && location.x <= 2 &&
+    location.y >= 0 && location.y <= 2
+  }
+}
+
+class DiamondKeypad() extends Keypad {
+  var location: Pair = new Pair(0, 2)
+
+  val keypad = Array(
+    Array("0", "0", "1", "0", "0"),
+    Array("0", "2", "3", "4", "0"),
+    Array("5", "6", "7", "8", "9"),
+    Array("0", "A", "B", "C", "0"),
+    Array("0", "0", "D", "0", "0")
+  )
+
+  def isValid(location: Pair): Boolean = {
+    location.x >= 0 && location.x <= 4 &&
+    location.y >= 0 && location.y <= 4 &&
+    keypad(location.y)(location.x) != "0"
+  }
+}
+
 object Solution {
-  def part1(lines: List[String]): Int = {
-    val keypad = Array(
-      Array(1, 2, 3),
-      Array(4, 5, 6),
-      Array(7, 8, 9)
-    )
-
-    val deltas = Map(
-      "U" -> new Pair(0, -1),
-      "R" -> new Pair(1, 0),
-      "D" -> new Pair(0, 1),
-      "L" -> new Pair(-1, 0)
-    )
-
-    lines
-      .foldLeft(Pair(1, 1), 0)((acc, line) => {
-        val (location, password) = acc
-        val newLocation = line.foldLeft(location)((currentLocation, c) => {
-          val newLocation = currentLocation + deltas(c.toString)
-          if (
-            newLocation.x < 0 || newLocation.x > 2 ||
-            newLocation.y < 0 || newLocation.y > 2
-          ) {
-            currentLocation
-          } else {
-            newLocation
-          }
-        })
-        (newLocation, password * 10 + keypad(newLocation.y)(newLocation.x))
-      })
-      ._2
-  }
-
-  def part2(lines: List[String]): String = {
-    val keypad = Array(
-      Array("0", "0", "1", "0", "0"),
-      Array("0", "2", "3", "4", "0"),
-      Array("5", "6", "7", "8", "9"),
-      Array("0", "A", "B", "C", "0"),
-      Array("0", "0", "D", "0", "0")
-    )
-
-    val deltas = Map(
-      "U" -> new Pair(0, -1),
-      "R" -> new Pair(1, 0),
-      "D" -> new Pair(0, 1),
-      "L" -> new Pair(-1, 0)
-    )
-
-    lines
-      .foldLeft(Pair(0, 2), "")((acc, line) => {
-        val (location, password) = acc
-        val newLocation = line.foldLeft(location)((currentLocation, c) => {
-          val newLocation = currentLocation + deltas(c.toString)
-          if (
-            newLocation.x < 0 || newLocation.x > 4 ||
-            newLocation.y < 0 || newLocation.y > 4 ||
-            keypad(newLocation.y)(newLocation.x) == "0"
-          ) {
-            currentLocation
-          } else {
-            newLocation
-          }
-        })
-        (newLocation, password + keypad(newLocation.y)(newLocation.x).toString)
-      })
-      ._2
-  }
-
   def main(args: Array[String]): Unit = {
     val lines = FileUtils.readFileContents(args(0))
-    println(s"Part 1: ${part1(lines)}")
-    println(s"Part 1: ${part2(lines)}")
+
+    val squareKeypad = new SquareKeypad()
+    val diamondKeypad = new DiamondKeypad()
+
+    lines.foreach(line => {
+      squareKeypad.move(line)
+      diamondKeypad.move(line)
+    })
+
+    println(s"Part 1: ${squareKeypad.password}")
+    println(s"Part 2: ${diamondKeypad.password}")
   }
 }
