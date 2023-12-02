c5716c2c24a1373ef7debb0a72156a05dcd68542
xlzior <wenjun.lye@gmail.com>
2023-12-02 00:49:55 +0000

    2016 Day 7

---
+++ 2016/day07/solution.scala
@@ -0,0 +1,36 @@
+import util.FileUtils
+
+object Solution {
+  val ABBA_PATTERN = """(\w)(?!\1)(\w)\2\1""".r
+  val BOXED_ABBA_PATTERN = """\[[^\]]*?(\w)(?!\1)(\w)\2\1[^\[]*\]""".r
+  val ABA_PATTERN = """(?=((\w)(?!\2)(\w)\2))""".r
+
+  def supportsTLS(s: String): Boolean = {
+    if (BOXED_ABBA_PATTERN.findFirstIn(s).isDefined) return false
+
+    ABBA_PATTERN.findFirstIn(s).isDefined
+  }
+
+  def supportsSSL(s: String): Boolean = {
+    val splitted = s.split(']').map(_.split('[')).transpose
+    val supernet = splitted(0)
+    val hypernet = splitted(1)
+
+    val supernetABA = supernet.flatMap(s => ABA_PATTERN.findAllMatchIn(s))
+    val hypernetABA =
+      hypernet.flatMap(h => ABA_PATTERN.findAllMatchIn(h).map(_.group(1)))
+
+    supernetABA.exists(aba => {
+      val a = aba.group(2)
+      val b = aba.group(3)
+      a != b && hypernetABA.contains(b + a + b)
+    })
+  }
+
+  def main(args: Array[String]): Unit = {
+    val lines = FileUtils.readFileContents(args(0))
+
+    println(s"Part 1: ${lines.count(supportsTLS)}")
+    println(s"Part 2: ${lines.count(supportsSSL)}")
+  }
+}
