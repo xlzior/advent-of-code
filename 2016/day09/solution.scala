411a78b617e5e934b35d918cf84dd9b0d93fc12f
xlzior <wenjun.lye@gmail.com>
2023-12-02 21:05:27 +0000

    2016 Day 9 Part 2

--- 2016/day09/solution.scala
+++ 2016/day09/solution.scala
@@ -2,9 +2,8 @@ import util.FileUtils
 
 object Solution {
   val pattern = """\((\d+)x(\d+)\)""".r
-  def main(args: Array[String]): Unit = {
-    val line = args(0)
 
+  def version1(line: String): Int = {
     var decompressed = ""
     var i = 0
     while (i < line.length()) {
@@ -13,18 +12,60 @@ object Solution {
         val length = marker.get.group(1).toInt
         val repetitions = marker.get.group(2).toInt
 
-        while (line(i) != ')') {
-          i += 1
-        }
+        while (line(i) != ')') i += 1
         i += 1 // skip ')'
         decompressed += line.substring(i).substring(0, length) * repetitions
-        i += length
+        i += length // skip the original
       } else {
         decompressed += line(i)
         i += 1
       }
     }
 
-    println(s"Part 1: ${decompressed.length()}")
+    decompressed.length()
+  }
+
+  def version2(line: String): Long = {
+    if (!line.contains('(')) {
+      // base case: no more decompression
+      return line.length()
+    }
+
+    // recursive case: contains a marker
+    val marker = pattern.findFirstMatchIn(line)
+    val length = marker.get.group(1).toInt
+    val repetitions = marker.get.group(2).toInt
+
+    val before = line.substring(0, marker.get.start)
+    val sequence = line.substring(marker.get.end).take(length)
+    val after = line.substring(marker.get.end + length)
+
+    return List(
+      version2(before),
+      version2(sequence) * repetitions,
+      version2(after)
+    ).sum
+  }
+
+  def main(args: Array[String]): Unit = {
+    val puzzle: String = FileUtils.readFileContents(args(0))(0)
+
+    assert(version1("ADVENT") == 6)
+    assert(version1("A(1x5)BC") == 7)
+    assert(version1("(3x3)XYZ") == 9)
+    assert(version1("A(2x2)BCD(2x2)EFG") == 11)
+    assert(version1("(6x1)(1x3)A") == 6)
+    assert(version1("X(8x2)(3x3)ABCY") == 18)
+    println(s"Part 1: ${version1(puzzle)}")
+
+    assert(version2("(3x3)XYZ") == 9)
+    assert(version2("X(8x2)(3x3)ABCY") == 20)
+    assert(version2("(27x12)(20x12)(13x14)(7x10)(1x12)A") == 241920)
+    assert(
+      version2(
+        "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN"
+      ) == 445
+    )
+    println(s"Part 2: ${version2(puzzle)}")
   }
 }
--- 2016/day09/test.sh
+++
@@ -1,8 +0,0 @@
-scala-cli ../util solution.scala -- "ADVENT"
-scala-cli ../util solution.scala -- "A(1x5)BC"
-scala-cli ../util solution.scala -- "(3x3)XYZ"
-scala-cli ../util solution.scala -- "A(2x2)BCD(2x2)EFG"
-scala-cli ../util solution.scala -- "(6x1)(1x3)A"
-scala-cli ../util solution.scala -- "X(8x2)(3x3)ABCY"
-
-scala-cli ../util solution.scala -- "$(cat puzzle.txt)"
