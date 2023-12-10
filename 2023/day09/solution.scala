184d1cd9f2da131a2519948784e888283e0096d5
xlzior <wenjun.lye@gmail.com>
2023-12-10 17:19:29 +0000

    2023 Day 9 add tests

--- 2023/day09/solution.scala
+++ 2023/day09/solution.scala
@@ -1,3 +1,4 @@
+import util.Solution
 import util.FileUtils
 
 def predict(line: List[Int]): Int = {
@@ -10,15 +11,21 @@ def predict(line: List[Int]): Int = {
   result
 }
 
-object Solution {
-  def main(args: Array[String]): Unit = {
-    val lines: List[String] = FileUtils.read(args(0))
+object Day9 extends Solution {
+  def solve(lines: List[String]): (Int, Int) = {
     val histories = lines.map(_.split(" ").map(_.toInt).toList)
 
     val part1 = histories.map(predict).sum
-    println(s"Part 1: $part1")
-
     val part2 = histories.map(_.reverse).map(predict).sum
+    (part1, part2)
+  }
+
+  def main(args: Array[String]): Unit = {
+    assert(testsPass)
+
+    val lines: List[String] = FileUtils.read("puzzle.in")
+    val (part1, part2) = solve(lines)
+    println(s"Part 1: $part1")
     println(s"Part 2: $part2")
   }
 }
