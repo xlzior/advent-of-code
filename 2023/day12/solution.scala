b00d455a90447011ab965bf844e32227afe067be
xlzior <wenjun.lye@gmail.com>
2023-12-12 20:03:33 +0000

    2023 Day 12 Part 2

--- 2023/day12/solution.scala
+++ 2023/day12/solution.scala
@@ -1,65 +1,79 @@
-import scala.collection.mutable.Set
 import scala.collection.mutable.Map
-import scala.collection.mutable.Queue
 
 import util.Solution
 import util.FileUtils
 
 object Day extends Solution {
-  def countGroups(condition: String): List[Int] = {
-    """(#+)""".r.findAllIn(condition).map(_.length).toList
+  val seen = Map[(String, List[Int]), Long]()
+
+  def couldBe(curr: String, goal: String): Boolean = {
+    curr.length == goal.length && curr.zip(goal).forall {
+      case ('.', '#') => false
+      case ('#', '.') => false
+      case _          => true
+    }
   }
 
-  def isPrefix(curr: List[Int], goal: List[Int]) = {
-    val n = curr.length
-    curr.take(n - 1) == goal.take(n - 1)
-  }
-
-  def countWays(condition: String, groups: List[Int]): Int = {
-    if (!condition.contains('?')) {
-      return if (countGroups(condition) == groups) 1 else 0
+  def countWays(condition: String, groups: List[Int]): Long = {
+    if (seen.contains((condition, groups))) {
+      return seen((condition, groups))
     }
 
-    val i = condition.indexOf("?")
+    var result = 0L
 
-    if (!isPrefix(countGroups(condition.slice(0, 1)), groups)) {
-      return 0
+    if (condition.length == 0 && groups.length == 0) {
+      result = 1L
+    } else if (condition.length == 0) {
+      result = 0L
+    } else if (groups.length == 0) {
+      result = if (condition.forall(_ != '#')) 1L else 0L
+    } else {
+      if (condition.head != '#') { // .
+        result += countWays(condition.slice(1, condition.length), groups)
+      }
+
+      if (condition.head != '.') { // #
+        val n = groups.head
+        if (couldBe(condition.slice(0, n + 1), "#".repeat(n) + ".")) {
+          result += countWays(
+            condition.slice(n + 1, condition.length),
+            groups.tail
+          )
+        }
+      }
     }
-
-    val broken = countWays(condition.updated(i, '#'), groups)
-    val working = countWays(condition.updated(i, '.'), groups)
-    broken + working
+    seen((condition, groups)) = result
+    result
   }
 
-  def part1(parsed: List[(String, List[Int])]): Int = {
+  def part1(parsed: List[(String, List[Int])]): Long = {
     parsed
-      .map((condition, groups) => countWays(condition, groups))
+      .map((condition, groups) => countWays(condition + ".", groups))
       .sum
   }
 
-  def part2(parsed: List[(String, List[Int])]): Int = {
+  def part2(parsed: List[(String, List[Int])]): Long = {
     parsed
       .map((condition, groups) => {
-        val expandedCondition = (condition + "?").repeat(5)
+        val expandedCondition = List.fill(5)(condition).mkString("?") + "."
         val expandedGroups = List.fill(5)(groups).flatten
         countWays(expandedCondition, expandedGroups)
       })
       .sum
   }
 
-  def solve(lines: List[String]): List[Int] = {
+  def solve(lines: List[String]): List[Long] = {
     val parsed = lines
       .map(_.split(" ").toList match {
         case List(condition, groups) =>
           (condition, groups.split(",").map(_.toInt).toList)
       })
 
-    List(part1(parsed))
-    // List(part1(parsed), part2(parsed))
+    List(part1(parsed), part2(parsed))
   }
 
   def main(args: Array[String]): Unit = {
-    // assert(testsPass)
+    assert(testsPass)
 
     val lines: List[String] = FileUtils.read(s"${args(0)}.in")
     val solution = solve(lines)
