88baeaf78bb10de396c5c87020da263011d218ce
xlzior <wenjun.lye@gmail.com>
2023-12-17 16:34:42 +0000

    2023 Day 17 Part 2

--- 2023/day17/solution.scala
+++ 2023/day17/solution.scala
@@ -8,55 +8,58 @@ import util.Grid
 import util.Pair
 
 object Day extends Solution {
-  val dirs = List(Pair(0, 1), Pair(1, 0), Pair(0, -1), Pair(-1, 0))
+  val up = Pair(-1, 0)
+  val down = Pair(1, 0)
+  val left = Pair(0, -1)
+  val right = Pair(0, 1)
+
+  val nextDirs = Map[Pair, List[Pair]](
+    up -> List(left, right),
+    down -> List(left, right),
+    left -> List(up, down),
+    right -> List(up, down)
+  )
 
   implicit val ordering: Ordering[(Int, Pair, List[Pair])] = Ordering.by(-_._1)
 
-  def part1(grid: Grid[Int]): Int = {
+  def crucible(grid: Grid[Int], min: Int, max: Int): Int = {
     val start = Pair(0, 0)
     val end = Pair(grid.h - 1, grid.w - 1)
-    val explored = Set[(Pair, List[Pair])]((start, List.empty[Pair]))
-    val pq =
-      PriorityQueue[(Int, Pair, List[Pair])]((0, start, List.empty[Pair]))
+    val dist = Map((start, List(right, down)) -> 0)
+    val pq = PriorityQueue((0, start, List(right, down)))
 
     while (pq.nonEmpty) {
-      val (heatLoss, u, path) = pq.dequeue()
+      val (heatLoss, curr, outDirs) = pq.dequeue()
 
-      if (u == end) {
+      if (curr == end)
         return heatLoss
-      }
 
-      dirs
-        .filter(dir => {
-          val goingBackwards = path.headOption.map(_ == -dir).getOrElse(false)
-          val fourthConsecutive =
-            path.length >= 3 && path.take(3).forall(_ == dir)
-          !goingBackwards && !fourthConsecutive
-        })
-        .foreach(dir => {
-          val v = u + dir
+      outDirs.foreach(dir => {
+        (min to max).foreach(i => {
+          val next = curr + dir * i
 
-          grid
-            .get(v)
-            .map(hl => {
-              if (!explored.contains((v, (dir :: path).take(3)))) {
-                explored.add((v, (dir :: path).take(3)))
-                pq.addOne((heatLoss + hl, v, dir :: path))
-              }
-            })
+          if (grid.contains(next)) {
+            val hl = (1 to i).map(j => grid.get(curr + dir * j).get).sum
+            val alt = heatLoss + hl
+
+            if (alt < dist.getOrElse((next, nextDirs(dir)), Int.MaxValue)) {
+              dist((next, nextDirs(dir))) = alt
+              pq.addOne((heatLoss + hl, next, nextDirs(dir)))
+            }
+          }
         })
+      })
     }
 
     -1
   }
 
-  def part2(lines: List[String]): Int = {
-    -1
-  }
+  def part1(grid: Grid[Int]): Int = crucible(grid, 1, 3)
+  def part2(grid: Grid[Int]): Int = crucible(grid, 4, 10)
 
   def solve(lines: List[String]): List[Int] = {
     val grid = Grid(lines.map(_.toCharArray().map(_.asDigit)).toArray)
-    List(part1(grid), part2(lines))
+    List(part1(grid), part2(grid))
   }
 
   def main(args: Array[String]): Unit = {
