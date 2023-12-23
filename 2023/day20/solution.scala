b1f515e92b4459c8a94fd1332f70b4d51ee613b0
xlzior <wenjun.lye@gmail.com>
2023-12-23 17:19:54 +0000

    2023 Day 20 Part 2

--- 2023/day20/solution.scala
+++ 2023/day20/solution.scala
@@ -1,4 +1,5 @@
 import scala.collection.mutable.Queue
+
 import util._
 
 enum Strength:
@@ -8,7 +9,9 @@ type Pulse = (String, Strength, String)
 
 trait Module:
   val name: String
+  val inputs: List[String]
   val outputs: List[String]
+  var history = Vector[Boolean]()
 
   def pulse(from: String, strength: Strength): List[Pulse] = {
     send(receive(from, strength))
@@ -22,7 +25,10 @@ trait Module:
       .getOrElse(List.empty)
   }
 
+  def log(): Unit = {}
+
 class FlipFlop(val name: String, val outputs: List[String]) extends Module {
+  val inputs = List.empty
   var on = false
 
   def receive(from: String, strength: Strength): Option[Strength] = {
@@ -34,7 +40,9 @@ class FlipFlop(val name: String, val outputs: List[String]) extends Module {
       }
   }
 
-  override def toString(): String = s"$name: $outputs"
+  override def log(): Unit = {
+    history = history.appended(on)
+  }
 }
 
 class Conjunction(
@@ -43,36 +51,46 @@ class Conjunction(
     val outputs: List[String]
 ) extends Module {
   var memory = inputs.map(i => (i, Strength.low)).toMap
+  var pulsedHigh = false
 
   def receive(from: String, strength: Strength): Option[Strength] = {
     memory = memory.updated(from, strength)
-    Some(
+    val outputStrength =
       if (memory.values.forall(_ == Strength.high)) Strength.low
       else Strength.high
-    )
+
+    pulsedHigh = pulsedHigh || outputStrength == Strength.high
+    Some(outputStrength)
   }
 
-  override def toString(): String = s"$name: $inputs | $outputs"
+  override def log(): Unit = {
+    history = history.appended(pulsedHigh)
+    pulsedHigh = false
+  }
 }
 
 class Broadcaster(val outputs: List[String]) extends Module {
   val name = "broadcaster"
+  val inputs = List.empty
   def receive(from: String, strength: Strength): Option[Strength] =
     Some(strength)
-
-  override def toString(): String = s"$name: $outputs"
 }
 
 class Test extends Module {
   val name = "output"
+  val inputs = List.empty
   val outputs = List.empty
   def receive(from: String, strength: Strength): Option[Strength] = None
 }
 
+def gcd(a: Long, b: Long): Long = if (b == 0) a else gcd(b, a % b)
+def lcm(a: Long, b: Long): Long = (a * b) / gcd(a, b)
+def lcm(numbers: Iterable[Long]): Long = numbers.reduce((a, b) => lcm(a, b))
+
 object Day extends Solution {
   val broadcaster = """(broadcaster) -> (.+)""".r
-  val flipflop = """%(\w+) -> (.+)""".r
-  val conjunction = """&(\w+) -> (.+)""".r
+  val flipflop = """%(.+) -> (.+)""".r
+  val conjunction = """&(.+) -> (.+)""".r
 
   def parseLine(line: String): (String, List[String]) = {
     val Array(n, o) = line.split(" -> ")
@@ -82,7 +100,7 @@ object Day extends Solution {
     (name, out)
   }
 
-  def conjInputs(conj: String, lines: List[String]): List[String] = {
+  def inputs(conj: String, lines: List[String]): List[String] = {
     val result = lines
       .flatMap(line => {
         val (name, out) = parseLine(line)
@@ -102,11 +120,30 @@ object Day extends Solution {
           case '&' =>
             (
               name,
-              Conjunction(name, conjInputs(name, lines), out)
+              Conjunction(name, inputs(name, lines), out)
             )
         }
       })
       .toMap
+  }
+
+  def pressButton(modules: Map[String, Module]): (Int, Int) = {
+    var low = 0
+    var high = 0
+    val pulses = Queue[Pulse](("button", Strength.low, "broadcaster"))
+
+    while (pulses.nonEmpty) {
+      val (from, strength, to) = pulses.dequeue()
+
+      strength match
+        case Strength.low  => low += 1
+        case Strength.high => high += 1
+
+      val nextPulses = modules(to).pulse(from, strength)
+      pulses.enqueueAll(nextPulses)
+    }
+
+    (low, high)
   }
 
   def part1(modules: Map[String, Module]): Int = {
@@ -114,35 +151,54 @@ object Day extends Solution {
     var high = 0
 
     for (i <- 1 to 1000) {
-      val pulses = Queue[Pulse](("button", Strength.low, "broadcaster"))
-
-      while (pulses.nonEmpty) {
-        val (from, strength, to) = pulses.dequeue()
-
-        // Part 1
-        strength match
-          case Strength.low  => low += 1
-          case Strength.high => high += 1
-
-        val nextPulses = modules(to).pulse(from, strength)
-        pulses.enqueueAll(nextPulses)
-      }
+      val (l, h) = pressButton(modules)
+      low += l
+      high += h
     }
 
     low * high
   }
 
-  def part2(modules: Map[String, Module]): Int = {
-    -1
+  def summarise(history: Vector[Boolean], n: Int): List[(Boolean, Int)] = {
+    (history, n) match {
+      case (_, 0)                => List.empty
+      case (v, _) if v.size == 0 => List.empty
+      case _ => {
+        val v = history.head
+        val section = history.takeWhile(_ == v)
+        val rest = history.dropWhile(_ == v)
+
+        (v, section.size) :: summarise(rest, n - 1)
+      }
+    }
   }
 
-  def solve(lines: List[String]): List[Int] = {
+  def part2(modules: Map[String, Module]): Long = {
+    for (i <- 1 to 10_000) {
+      pressButton(modules)
+      modules.values.foreach(_.log())
+    }
+
+    val histories = modules
+      .filter((key, value) => {
+        // TODO: don't hardcode this
+        modules("lg").inputs.contains(key)
+      })
+      .map((name, module) => {
+        summarise(module.history, 2).map(_._2).sum.toLong
+      })
+
+    lcm(histories)
+  }
+
+  def solve(lines: List[String]): List[Long] = {
     val modules = parse(lines).updated("output", Test()).updated("rx", Test())
-    List(part1(modules), part2(modules))
+    val modules2 = parse(lines).updated("output", Test()).updated("rx", Test())
+    List(part1(modules).toLong, part2(modules2))
   }
 
   def main(args: Array[String]): Unit = {
-    assert(testsPass)
+    // assert(testsPass)
 
     val lines: List[String] = FileUtils.read(s"${args(0)}.in")
     val solution = solve(lines)
