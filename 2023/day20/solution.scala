973b59ec1e070f00b19a3179db572648b528cacc
xlzior <wenjun.lye@gmail.com>
2023-12-23 22:07:33 +0000

    2023 Day 20 refactor

--- 2023/day20/solution.scala
+++ 2023/day20/solution.scala
@@ -2,10 +2,7 @@ import scala.collection.mutable.Queue
 
 import util._
 
-enum Strength:
-  case low, high
-
-type Pulse = (String, Strength, String)
+type Pulse = (String, Boolean, String)
 
 trait Module:
   val name: String
@@ -13,17 +10,13 @@ trait Module:
   val outputs: List[String]
   var history = Vector[Boolean]()
 
-  def pulse(from: String, strength: Strength): List[Pulse] = {
-    send(receive(from, strength))
-  }
-
-  def receive(from: String, strength: Strength): Option[Strength]
-
-  def send(strength: Option[Strength]): List[Pulse] = {
-    strength
+  def pulse(from: String, isHigh: Boolean): List[Pulse] = {
+    receive(from, isHigh)
       .map(s => outputs.map(to => (name, s, to)))
       .getOrElse(List.empty)
   }
+
+  def receive(from: String, isHigh: Boolean): Option[Boolean]
 
   def log(): Unit = {}
 
@@ -31,12 +24,12 @@ class FlipFlop(val name: String, val outputs: List[String]) extends Module {
   val inputs = List.empty
   var on = false
 
-  def receive(from: String, strength: Strength): Option[Strength] = {
-    strength match
-      case Strength.high => None
-      case Strength.low => {
+  def receive(from: String, isHigh: Boolean): Option[Boolean] = {
+    isHigh match
+      case true => None
+      case false => {
         on = !on
-        Some(if (on) Strength.high else Strength.low)
+        Some(on)
       }
   }
 
@@ -50,17 +43,14 @@ class Conjunction(
     val inputs: List[String],
     val outputs: List[String]
 ) extends Module {
-  var memory = inputs.map(i => (i, Strength.low)).toMap
+  var memory = inputs.map(i => (i, false)).toMap
   var pulsedHigh = false
 
-  def receive(from: String, strength: Strength): Option[Strength] = {
-    memory = memory.updated(from, strength)
-    val outputStrength =
-      if (memory.values.forall(_ == Strength.high)) Strength.low
-      else Strength.high
-
-    pulsedHigh = pulsedHigh || outputStrength == Strength.high
-    Some(outputStrength)
+  def receive(from: String, isHigh: Boolean): Option[Boolean] = {
+    memory = memory.updated(from, isHigh)
+    val pulse = memory.values.exists(!_)
+    pulsedHigh ||= pulse
+    Some(pulse)
   }
 
   override def log(): Unit = {
@@ -72,15 +62,15 @@ class Conjunction(
 class Broadcaster(val outputs: List[String]) extends Module {
   val name = "broadcaster"
   val inputs = List.empty
-  def receive(from: String, strength: Strength): Option[Strength] =
-    Some(strength)
+  def receive(from: String, isHigh: Boolean): Option[Boolean] =
+    Some(isHigh)
 }
 
 class Test extends Module {
   val name = "output"
   val inputs = List.empty
   val outputs = List.empty
-  def receive(from: String, strength: Strength): Option[Strength] = None
+  def receive(from: String, isHigh: Boolean): Option[Boolean] = None
 }
 
 def gcd(a: Long, b: Long): Long = if (b == 0) a else gcd(b, a % b)
@@ -88,58 +78,46 @@ def lcm(a: Long, b: Long): Long = (a * b) / gcd(a, b)
 def lcm(numbers: Iterable[Long]): Long = numbers.reduce((a, b) => lcm(a, b))
 
 object Day extends Solution {
-  val broadcaster = """(broadcaster) -> (.+)""".r
-  val flipflop = """%(.+) -> (.+)""".r
-  val conjunction = """&(.+) -> (.+)""".r
-
-  def parseLine(line: String): (String, List[String]) = {
-    val Array(n, o) = line.split(" -> ")
-    val name = n.stripMargin('%').stripMargin('&')
-    val out = o.split(", ").toList
-
-    (name, out)
+  def splitLine(line: String): (String, List[String]) = {
+    val Array(name, o) = line.split(" -> ")
+    (name.stripMargin('%').stripMargin('&'), o.split(", ").toList)
   }
 
-  def inputs(conj: String, lines: List[String]): List[String] = {
-    val result = lines
-      .flatMap(line => {
-        val (name, out) = parseLine(line)
-        if (out.contains(conj)) Option(name) else None
-      })
-    result
-  }
+  def inputs(conj: String, lines: List[String]): List[String] = lines
+    .flatMap(line => {
+      val (name, out) = splitLine(line)
+      if (out.contains(conj)) Option(name) else None
+    })
 
-  def parse(lines: List[String]): Map[String, Module] = {
-    lines
-      .map(line => {
-        val (name, out) = parseLine(line)
+  def parse(lines: List[String]): Map[String, Module] = lines
+    .map(line => {
+      val (name, out) = splitLine(line)
 
-        line(0) match {
-          case 'b' => (name, Broadcaster(out))
-          case '%' => (name, FlipFlop(name, out))
-          case '&' =>
-            (
-              name,
-              Conjunction(name, inputs(name, lines), out)
-            )
-        }
-      })
-      .toMap
-  }
+      line(0) match {
+        case 'b' => (name, Broadcaster(out))
+        case '%' => (name, FlipFlop(name, out))
+        case '&' =>
+          (
+            name,
+            Conjunction(name, inputs(name, lines), out)
+          )
+      }
+    })
+    .toMap
 
   def pressButton(modules: Map[String, Module]): (Int, Int) = {
     var low = 0
     var high = 0
-    val pulses = Queue[Pulse](("button", Strength.low, "broadcaster"))
+    val pulses = Queue[Pulse](("button", false, "broadcaster"))
 
     while (pulses.nonEmpty) {
-      val (from, strength, to) = pulses.dequeue()
+      val (from, isHigh, to) = pulses.dequeue()
 
-      strength match
-        case Strength.low  => low += 1
-        case Strength.high => high += 1
+      isHigh match
+        case false => low += 1
+        case true  => high += 1
 
-      val nextPulses = modules(to).pulse(from, strength)
+      val nextPulses = modules(to).pulse(from, isHigh)
       pulses.enqueueAll(nextPulses)
     }
 
@@ -173,32 +151,32 @@ object Day extends Solution {
     }
   }
 
-  def part2(modules: Map[String, Module]): Long = {
+  def part2(modules: Map[String, Module], target: String): Long = {
     for (i <- 1 to 10_000) {
       pressButton(modules)
       modules.values.foreach(_.log())
     }
 
     val histories = modules
-      .filter((key, value) => {
-        // TODO: don't hardcode this
-        modules("lg").inputs.contains(key)
-      })
-      .map((name, module) => {
-        summarise(module.history, 2).map(_._2).sum.toLong
-      })
+      .filter((key, value) => modules(target).inputs.contains(key))
+      .map((name, module) => summarise(module.history, 2).map(_._2).sum.toLong)
 
     lcm(histories)
   }
 
   def solve(lines: List[String]): List[Long] = {
     val modules = parse(lines).updated("output", Test()).updated("rx", Test())
-    val modules2 = parse(lines).updated("output", Test()).updated("rx", Test())
-    List(part1(modules).toLong, part2(modules2))
+
+    if (lines.exists(_.contains("rx"))) {
+      val modules2 = parse(lines).updated("rx", Test())
+      List(part1(modules).toLong, part2(modules2, inputs("rx", lines)(0)))
+    } else {
+      List(part1(modules).toLong)
+    }
   }
 
   def main(args: Array[String]): Unit = {
-    // assert(testsPass)
+    assert(testsPass)
 
     val lines: List[String] = FileUtils.read(s"${args(0)}.in")
     val solution = solve(lines)
