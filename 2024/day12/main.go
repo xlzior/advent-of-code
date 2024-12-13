2917ec8a9d05f7906539040c139673d0cd4ca2e1
xlzior <wenjun.lye@gmail.com>
2024-12-13 22:56:16 +0800

    2024 Day 12 Part 2

--- 2024/day12/main.go
+++ 2024/day12/main.go
@@ -6,11 +6,9 @@ import (
 	"github.com/xlzior/aoc2024/utils"
 )
 
-func main() {
-	lines := utils.ReadLines()
-	grid := utils.Grid{Grid: lines}
-	nsew := []utils.Pair{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
+var nsew = []utils.Pair{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
 
+func getAllCells(grid utils.Grid) map[utils.Pair]bool {
 	todo := make(map[utils.Pair]bool)
 	br := grid.GetBottomRight()
 	for i := 0; i < br.R; i++ {
@@ -18,38 +16,86 @@ func main() {
 			todo[utils.Pair{R: i, C: j}] = true
 		}
 	}
+	return todo
+}
 
-	part1 := 0
-	for len(todo) > 0 {
-		for p := range todo {
-			plant := grid.GetCell(p)
-			area := 0
-			perimeter := 0
+func head(m map[utils.Pair]bool) utils.Pair {
+	for p := range m {
+		return p
+	}
+	panic("map is empty")
+}
 
-			visited := make(map[utils.Pair]bool)
-			neighbours := []utils.Pair{p}
-			for len(neighbours) > 0 {
-				curr := neighbours[0]
-				neighbours = neighbours[1:]
-				if grid.GetCell(curr) == plant && !visited[curr] {
-					visited[curr] = true
-					delete(todo, curr)
-					area++
-					for _, dir := range nsew {
-						next := curr.Plus(dir)
-						if !visited[next] {
-							neighbours = append(neighbours, next)
-						}
-					}
-				}
-				if grid.GetCell(curr) != plant {
-					perimeter++
+func countConnectedComponents(nodes map[utils.Pair]bool) int {
+	count := 0
+	for len(nodes) > 0 {
+		component := []utils.Pair{head(nodes)}
+		for len(component) > 0 {
+			curr := component[0]
+			component = component[1:]
+			delete(nodes, curr)
+			for _, dir := range nsew {
+				next := curr.Plus(dir)
+				if nodes[next] {
+					component = append(component, next)
 				}
 			}
-			fmt.Println(string(plant), area, perimeter)
-			part1 += area * perimeter
-			break
 		}
+		count++
 	}
+	return count
+}
+
+func solve(grid utils.Grid) (int, int) {
+	todo := getAllCells(grid)
+
+	part1 := 0
+	part2 := 0
+	for len(todo) > 0 {
+		p := head(todo)
+		plant := grid.GetCell(p)
+		area := 0
+		perimeter := make(map[utils.Pair]map[utils.Pair]bool)
+
+		visited := make(map[utils.Pair]bool)
+		neighbours := []utils.Pair{p}
+		for len(neighbours) > 0 {
+			curr := neighbours[0]
+			neighbours = neighbours[1:]
+			if grid.GetCell(curr) == plant && !visited[curr] {
+				visited[curr] = true
+				delete(todo, curr)
+				area++
+				for _, dir := range nsew {
+					next := curr.Plus(dir)
+					if grid.GetCell(next) != plant {
+						if perimeter[dir] == nil {
+							perimeter[dir] = make(map[utils.Pair]bool)
+						}
+						perimeter[dir][next] = true
+					} else if !visited[next] {
+						neighbours = append(neighbours, next)
+					}
+				}
+			}
+		}
+		fences := 0
+		sides := 0
+		for dir := range perimeter {
+			dirFences := perimeter[dir]
+			fences += len(dirFences)
+			sides += countConnectedComponents(dirFences)
+		}
+		part1 += area * fences
+		part2 += area * sides
+	}
+	return part1, part2
+}
+
+func main() {
+	lines := utils.ReadLines()
+	grid := utils.Grid{Grid: lines}
+	part1, part2 := solve(grid)
 	fmt.Println("Part 1:", part1)
+	fmt.Println("Part 2:", part2)
 }
