17ca3f92ba1824e4bf7f2cce151e516b38a6ee53
xlzior <wenjun.lye@gmail.com>
2024-12-29 19:23:35 +0800

    2024 Day 15 refactor

--- 2024/day15/main.go
+++ 2024/day15/main.go
@@ -14,68 +14,50 @@ var dirs = map[rune]utils.Pair{
 	'v': {R: 1, C: 0},
 }
 
-func pushBoxes(boxes, walls map[utils.Pair]bool, box, dir utils.Pair) ([]utils.Pair, []utils.Pair, bool) {
+func canPushBoxes(grid utils.Grid, box, dir utils.Pair) bool {
 	next := box.Plus(dir)
-	boxesPushed := []utils.Pair{box}
-	newBoxes := []utils.Pair{next}
-	blocked := false
-
-	if walls[next] {
-		return boxesPushed, newBoxes, true
+	if grid.GetCell(next) == '#' { // wall
+		return false
+	} else if grid.GetCell(next) == 'O' { // box
+		return canPushBoxes(grid, next, dir)
+	} else { // empty space
+		return true
 	}
-	if boxes[next] {
-		old, new, b := pushBoxes(boxes, walls, next, dir)
-		boxesPushed = append(boxesPushed, old...)
-		newBoxes = append(newBoxes, new...)
-		blocked = blocked || b
-	}
-	return boxesPushed, newBoxes, blocked
 }
 
-func printGrid(curr utils.Pair, grid utils.Grid, walls, boxes map[utils.Pair]bool) {
-	for i := 0; i < grid.NumRows(); i++ {
-		for j := 0; j < grid.NumCols(); j++ {
-			p := utils.Pair{R: i, C: j}
-			if p == curr {
-				fmt.Print("@")
-			} else if walls[p] {
-				fmt.Print("#")
-			} else if boxes[p] {
-				fmt.Print("O")
-			} else {
-				fmt.Print(" ")
-			}
-		}
-		fmt.Println()
+func pushBoxes(grid utils.Grid, box, dir utils.Pair) bool {
+	next := box.Plus(dir)
+	pushed := true
+
+	if grid.GetCell(next) == '#' { // wall
+		return false
+	} else if grid.GetCell(next) == 'O' { // box
+		pushed = pushBoxes(grid, next, dir)
 	}
+	if pushed {
+		grid.SetCell(next, 'O')
+		grid.SetCell(box, '.')
+	}
+	return pushed
 }
 
 func solvePart1(grid utils.Grid, instrs string) map[utils.Pair]bool {
 	curr := grid.FindAllList('@')[0]
-	walls := grid.FindAllSet('#')
-	boxes := grid.FindAllSet('O')
 	for _, i := range instrs {
 		next := curr.Plus(dirs[i])
-		if walls[next] {
+		pushed := true
+		if grid.GetCell(next) == '#' {
 			continue
-		} else if boxes[next] {
-			boxesPushed, newBoxes, blocked := pushBoxes(boxes, walls, next, dirs[i])
-			if blocked {
-				continue
-			} else {
-				for _, box := range boxesPushed {
-					delete(boxes, box)
-				}
-				for _, box := range newBoxes {
-					boxes[box] = true
-				}
-				curr = next
-			}
-		} else { // empty space
+		} else if grid.GetCell(next) == 'O' {
+			pushed = pushBoxes(grid, next, dirs[i])
+		}
+		if pushed {
+			grid.SetCell(curr, '.')
+			grid.SetCell(next, '@')
 			curr = next
 		}
 	}
-	return boxes
+	return grid.FindAllSet('O')
 }
 
 func sumGpsCoordinates(boxes map[utils.Pair]bool) int {
