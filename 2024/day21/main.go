922c9494c43a3e72b641a9192bfda717f415a912
xlzior <wenjun.lye@gmail.com>
2025-01-01 14:51:01 +0800

    2024 Day 21 Part 1

--- 2024/day21/main.go
+++ 2024/day21/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"slices"
 	"strings"
 
 	"github.com/xlzior/aoc2024/utils"
@@ -13,7 +14,7 @@ var numpad = make(map[string]utils.Pair)
 var dirpad = make(map[string]utils.Pair)
 
 func complexity(code string, sequence string) int {
-	fmt.Println(len(sequence), utils.MustParseInt(code[:len(code)-1]))
+	fmt.Println(code, len(sequence), utils.MustParseInt(code[:len(code)-1]), sequence)
 	return len(sequence) * utils.MustParseInt(code[:len(code)-1])
 }
 
@@ -38,37 +39,6 @@ var unitRight = unit{
 	condition: func(p utils.Pair) bool { return p.C > 0 },
 	generator: func(p utils.Pair) string { return strings.Repeat(">", p.C) },
 }
-var numpadUnits = []unit{unitUp, unitRight, unitDown, unitLeft}
-var dirpadUnits = []unit{unitRight, unitDown, unitLeft, unitUp}
-
-func expandSingle(delta utils.Pair, units []unit) string {
-	result := make([]string, 0)
-	for _, u := range units {
-		if u.condition(delta) {
-			result = append(result, u.generator(delta))
-		}
-	}
-	return strings.Join(result, "")
-}
-
-func expandSequence(pad map[string]utils.Pair, sequence string) string {
-	result := make([]string, 0)
-	sequence = "A" + sequence
-	var units []unit
-	if len(pad) == len(numpad) {
-		units = numpadUnits
-	} else {
-		units = dirpadUnits
-	}
-	for i := 0; i < len(sequence)-1; i++ {
-		c := pad[string(sequence[i])]
-		n := pad[string(sequence[i+1])]
-		delta := n.Minus(c)
-
-		result = append(result, expandSingle(delta, units), "A")
-	}
-	return strings.Join(result, "")
-}
 
 var dirmap = map[rune]utils.Pair{
 	'>': {R: 0, C: 1},
@@ -91,19 +61,82 @@ func contract(pad []string, sequence string, start utils.Pair) string {
 	return strings.Join(result, "")
 }
 
-func getButtonSequence(code string) string {
-	first := expandSequence(numpad, code)
-	fmt.Println(first)
-	second := expandSequence(dirpad, first)
-	fmt.Println(second)
-	third := expandSequence(dirpad, second)
-	fmt.Println(third)
-	return third
+func isIllegal(start string, pad map[string]utils.Pair, sequence string) bool {
+	illegal := pad[" "]
+	curr := pad[start]
+	for _, s := range sequence {
+		if curr == illegal {
+			return true
+		}
+		curr = curr.Plus(dirmap[s])
+	}
+	return curr == illegal
+}
+
+func getPermutations(delta utils.Pair) []string {
+	segments := make([]string, 0)
+	for _, u := range []unit{unitUp, unitRight, unitDown, unitLeft} {
+		if u.condition(delta) {
+			segments = append(segments, u.generator(delta))
+		}
+	}
+	seq := strings.Join(segments, "") + "A"
+	if len(segments) <= 1 {
+		return []string{seq}
+	} else if len(segments) == 2 {
+		slices.Reverse(segments)
+		return []string{seq, strings.Join(segments, "") + "A"}
+	} else {
+		panic("expected only 1 or 2 segments")
+	}
+}
+
+func getShortest(strs []string) string {
+	shortest := strs[0]
+	for _, s := range strs {
+		if len(s) < len(shortest) {
+			shortest = s
+		}
+	}
+	return shortest
+}
+
+func decompileSegment(pad map[string]utils.Pair, start, end string, n int) string {
+	delta := pad[end].Minus(pad[start])
+	perms := getPermutations(delta)
+
+	candidates := []string{}
+	for _, perm := range perms {
+		if isIllegal(start, pad, perm) {
+			continue
+		}
+		if n == 0 {
+			candidates = append(candidates, perm)
+		} else {
+			candidates = append(candidates, decompileSequence(dirpad, perm, n-1))
+		}
+	}
+	if len(candidates) == 1 {
+		return candidates[0]
+	} else {
+		return getShortest(candidates)
+	}
+}
+
+func decompileSequence(pad map[string]utils.Pair, code string, n int) string {
+	result := []string{}
+	code = "A" + code
+	for i := 0; i < len(code)-1; i++ {
+		curr := string(code[i])
+		next := string(code[i+1])
+		seq := decompileSegment(pad, curr, next, n)
+		result = append(result, seq)
+	}
+	return strings.Join(result, "")
 }
 
 func main() {
 	lines := utils.ReadLines()
-	part1 := 0
 	for r, row := range nums {
 		for c, cell := range row {
 			numpad[string(cell)] = utils.Pair{R: r, C: c}
@@ -115,8 +148,9 @@ func main() {
 		}
 	}
 
+	part1 := 0
 	for _, code := range lines {
-		seq := getButtonSequence(code)
+		seq := decompileSequence(numpad, code, 2)
 		part1 += complexity(code, seq)
 	}
 	fmt.Println("Part 1:", part1)
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 <!-- AOC TILES BEGIN -->
 <h1 align="center">
-  Advent of Code - 325/500 ⭐
+  Advent of Code - 326/500 ⭐
 </h1>
 <h1 align="center">
-  2024 - 47 ⭐ - Go
+  2024 - 48 ⭐ - Go
 </h1>
 <a href="2024/day01/main.go">
   <img src=".aoc_tiles/tiles/2024/01.png" width="161px">
@@ -65,7 +65,7 @@
 <a href="2024/day20/main.go">
   <img src=".aoc_tiles/tiles/2024/20.png" width="161px">
 </a>
-<a href="None">
+<a href="2024/day21/main.go">
   <img src=".aoc_tiles/tiles/2024/21.png" width="161px">
 </a>
 <a href="2024/day22/main.go">
