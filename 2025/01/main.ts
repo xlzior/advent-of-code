966484f61eef0f38d9e9682616435340d6a53dba
xlzior <wenjun.lye@gmail.com>
2025-12-01 22:58:14 +0800

    2025 Day 1

--- 2025/01/main.ts
+++ 2025/01/main.ts
@@ -1,4 +1,30 @@
 import { readLines } from "../utils";
 
+function day01(input: string[]) {
+  let part1 = 0;
+  let part2 = 0;
+  let curr = 50;
+
+  for (const line of input) {
+    const direction = line[0] == "R" ? 1 : -1;
+    const steps = parseInt(line.substring(1));
+    let next = curr + direction * steps;
+
+    if (next % 100 == 0) {
+      part1++;
+    }
+    for (let i = curr; i != next; i += direction) {
+      if (i % 100 == 0) {
+        part2++;
+      }
+    }
+    curr = next;
+  }
+
+  return [part1, part2];
+}
+
 const input = await readLines();
-console.log(input);
+const [part1, part2] = day01(input);
+console.log("Part 1:", part1);
+console.log("Part 2:", part2);
