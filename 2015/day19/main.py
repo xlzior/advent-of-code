ecd5255a3ce4d8d5ece07d4ec4ba0986deb8fc53
xlzior <wenjun.lye@gmail.com>
2025-12-07 20:03:40 +0800

    2015 Day 19 part 2 feeble attempt

--- 2015/day19/main.py
+++ 2015/day19/main.py
@@ -1,3 +1,5 @@
+from functools import cache
+import math
 import sys
 import re
 
@@ -17,3 +19,79 @@ for before, after in replacements:
         molecules.add(replace(medicine, m, after))
 
 print("Part 1:", len(molecules))
+
+
+seen = set()
+
+
+@cache
+def get_num_steps(medicine, target):
+    seen.add(medicine)
+    if medicine == target:
+        return 0
+
+    num_steps = []
+    for before, after in replacements:
+        matches = re.finditer(f"({after})", medicine)
+        for m in matches:
+            molecule = replace(medicine, m, before)
+            num_steps.append(get_num_steps(molecule, target) + 1)
+
+    result = min(num_steps) if num_steps else math.inf
+    return result
+
+
+def count_atoms(medicine):
+    return len([c for c in medicine if c.upper() == c])
+
+
+def split_atoms(medicine):
+    atom_starts = [i for (i, c) in enumerate(medicine) if c.upper() == c]
+    atom_starts.append(len(medicine))
+    return [
+        medicine[atom_starts[i] : atom_starts[i + 1]]
+        for i in range(len(atom_starts) - 1)
+    ]
+
+
+# print("Part 2:", get_num_steps(medicine, "e"))
+
+
+@cache
+def eliminate_argon(medicine):
+    shrinks = set()
+    if count_atoms(medicine) == 1:
+        return medicine
+    for before, after in replacements:
+        matches = re.finditer(f"({after})", medicine)
+        for m in matches:
+            molecule = replace(medicine, m, before)
+            if "Ar" in molecule:
+                shrinks.update(
+                    [(mol, cost + 1) for mol, cost in eliminate_argon(molecule)]
+                )
+            else:
+                shrinks.add((molecule, 1))
+    return shrinks
+
+
+@cache
+def algorithm(medicine):
+    if "Ar" not in medicine:
+        print("no argon found?", medicine)
+        return [(medicine, 0)]
+
+    segment, rest = medicine.split("Ar", 1)
+    segment += "Ar"
+
+    no_argons = eliminate_argon(segment)
+    results = []
+    for replacement, cost in no_argons:
+        subresults = algorithm(replacement + rest)
+        results.extend((result, cost + subcost) for result, subcost in subresults)
+    return results
+
+
+results = algorithm(medicine)
+medicine, cost = results[0]
+print(medicine, cost)
