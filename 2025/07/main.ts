2b350e797c11ad1c26ee260fa5616f34d191b016
xlzior <wenjun.lye@gmail.com>
2025-12-07 15:31:30 +0800

    2025 Day 7 refactor

--- 2025/07/main.ts
+++ 2025/07/main.ts
@@ -2,53 +2,43 @@ import { readLines } from "../utils";
 import util from "node:util";
 
 class Point {
-  row: number;
-  col: number;
-
-  private static pool = new Map<string, Point>();
-  private constructor(row: number, col: number) {
-    this.row = row;
-    this.col = col;
+  constructor(readonly row: number, readonly col: number) {
   }
 
-  [util.inspect.custom]() {
+  key() {
     return `(${this.row},${this.col})`;
   }
 
-  static of(row: number, col: number): Point {
-    const hash = `${row}:${col}`;
-    if (!this.pool.has(hash)) {
-      this.pool.set(hash, new Point(row, col));
-    }
-    return this.pool.get(hash)!;
+  [util.inspect.custom]() {
+    return this.key;
   }
 
   public add(other: Point): Point {
-    return Point.of(this.row + other.row, this.col + other.col);
+    return new Point(this.row + other.row, this.col + other.col);
   }
 }
 
-const down = Point.of(1, 0);
-const left = Point.of(0, -1);
-const right = Point.of(0, 1);
+const down = new Point(1, 0);
+const left = new Point(0, -1);
+const right = new Point(0, 1);
 
 class Manifold {
   height: number;
   start: Point;
-  splitters: Set<Point>;
-  memo: Map<Point, number>;
+  splitters: Set<string>;
+  memo: Map<string, number>;
 
   constructor(input: string[]) {
     this.height = input.length;
     const startCol = input[0].indexOf("S");
-    this.start = Point.of(0, startCol);
-    this.splitters = new Set<Point>();
-    this.memo = new Map<Point, number>();
+    this.start = new Point(0, startCol);
+    this.splitters = new Set<string>();
+    this.memo = new Map<string, number>();
 
     input.forEach((row, r) => {
       row.split("").forEach((col, c) => {
         if (col == "^") {
-          this.splitters.add(Point.of(r, c));
+          this.splitters.add(new Point(r, c).key());
         }
       });
     });
@@ -56,20 +46,20 @@ class Manifold {
 
   countSplits() {
     let numSplits = 0;
-    const visited = new Set<Point>();
+    const visited = new Set<string>();
     const stack = [this.start];
 
     const addToStack = (beam: Point) => {
-      if (visited.has(beam) || beam.row > this.height) {
+      if (visited.has(beam.key()) || beam.row > this.height) {
         return;
       }
-      visited.add(beam);
+      visited.add(beam.key());
       stack.push(beam);
     };
     while (stack.length > 0) {
       const beam = stack.pop()!;
       const belowBeam = beam.add(down);
-      if (this.splitters.has(belowBeam)) {
+      if (this.splitters.has(belowBeam.key())) {
         numSplits++;
         for (const dir of [left, right]) {
           addToStack(belowBeam.add(dir));
@@ -84,16 +74,16 @@ class Manifold {
   countTimelines(curr: Point): number {
     if (curr.row > this.height) {
       return 1;
-    } else if (this.memo.has(curr)) {
-      return this.memo.get(curr)!;
-    } else if (this.splitters.has(curr)) {
+    } else if (this.memo.has(curr.key())) {
+      return this.memo.get(curr.key())!;
+    } else if (this.splitters.has(curr.key())) {
       const result = this.countTimelines(curr.add(left)) +
         this.countTimelines(curr.add(right));
-      this.memo.set(curr, result);
+      this.memo.set(curr.key(), result);
       return result;
     } else {
       const result = this.countTimelines(curr.add(down));
-      this.memo.set(curr, result);
+      this.memo.set(curr.key(), result);
       return result;
     }
   }
